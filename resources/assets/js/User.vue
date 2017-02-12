67893b2758d6f2b5b8f47a2671951deeb53b7555
MorkHub <mork@themork.co.uk>
2017-02-12 16:19:00 +0000

    adding parsing to scrobbles, tweaked user/now playing section

--- resources/assets/js/User.vue
+++ resources/assets/js/User.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="row">
+  <div class="row" v-if="userData.user == undefined || userData.user.name == ''">
     <div class="panel panel-default" style="background: rgba(255,255,255,.7)">
       <div class="panel-heading">User</div>
       <div class="panel-body">
@@ -13,23 +13,25 @@
 
   <div class="row" v-if="userData.user.name !== ''">
     <div class="col-xs-12">
+
       <div class="well" style="background: rgba(255,255,255,.7)">
-  	    <img id="avatar" width="auto" height="100%" style="border-radius:50%; float:right; clear:both; max-height: 200px" :src="userData.user.image[2]['#text']">
-        <h1 id="user">Username: <a target="_blank" :href="userData.user.url">{{userData.user.name}}</a></h1>
+  	    <img id="avatar" width="auto" height="100%" style="border-radius:50%; float:right; clear:both; max-height: 150px" :src="userData.user.image[2]['#text']">
+        <h1 id="user"><a target="_blank" :href="userData.user.url">{{userData.user.name}}</a></h1>
   	    <h2 id="played">This user has played {{userData.user.playcount}} tracks!
           <span>That's {{ Math.floor(userData.user.playcount / ((Date.now() - 1000*parseInt(userData.user.registered.unixtime))/1000/3600)*10)/10 }} per hour.</span>
         </h2>
   	    <h2 id="nowplaying">They are currently listening to: {{user.nowPlaying.name}} by {{user.nowPlaying.artist['#text']}}</h2>
-  	    <h3 id="scrobbled" v-if="user.scrobbled.track_name != ''">They last scrobbled: {{user.scrobbled.track_name}}</h3>
+  	    <h3 id="scrobbled" v-if="user.scrobbled.name != 'Nothing!'">They last scrobbled: {{user.scrobbled.name}} by {{ user.scrobbled.artist['#text'] }}</h3>
 
       </div>
     </div>
   </div>
 
-  <div class="row" v-if="userData.user.name !== ''">
+  <div class="row" v-if="userData.user.name !== '' && false">
     <div class="well" style="background: rgba(255,255,255,.7)">
       <pre><code>{{ userData | json }}</code></pre>
-      <prev v-if="window.location.hash.indexOf('debug') != -1"><code>{{ user.nowPlaying | json }}</code></pre>
+      <pre><code>{{ user.nowPlaying | json }}</code></pre>
+      <pre><code>{{ user.scrobbled | json }}</code></pre>
     </div>
   </div>
 </template>
@@ -44,7 +46,7 @@
                 track_name: 'Nothing!'
               },
               scrobbled: {
-                track_name: 'Nothing!'
+                name: 'Nothing!'
               },
             },
             userData: {}
@@ -66,61 +68,32 @@
               this.source.close();
             }
 
-            this.source = new EventSource('http://barno.org:3000/lastfm/user-update-stream?username=' + username);
+            this.source = new EventSource('https://barno.org:3000/lastfm/user-update-stream?username=' + username);
             this.source.addEventListener('user.nowPlaying', (e) => {
+							console.log(e.data);
               let playingInfo = JSON.parse(e.data);
               this.user.nowPlaying = JSON.parse(playingInfo.track);
-              document.body.style = "padding-top: 60px; background:url('"+this.user.nowPlaying.image[0]['#text']+"');background-attachment:fixed;background-size:cover;background-position";
+              let images = this.user.nowPlaying.image;
+              document.body.style = "padding-top: 60px; background:url('"+images[images.length -1]['#text']+"');background-attachment:fixed;background-size:cover;background-position";
             }, false);
 
             this.source.addEventListener('user.scrobbled', (e) => {
+							console.log(e.data);
               let playingInfo = JSON.parse(e.data);
-              this.user.scrobbled = playingInfo;
+              this.user.scrobbled = JSON.parse(playingInfo.track);
             }, false);
           } else {
             // Result to xhr polling :(
           }
 
-          this.$http.get('http://barno.org:3000/lastfm', {
+          this.$http.get('https://barno.org:3000/lastfm', {
             params: {
               'user': username,
               'type': 'user.getInfo'
             }
           }).then(response => {
 
-            this.userData = response.body;
-
-          }, response => {
-            // error callback
-          });
-        }
-
-
-      }
-    }
-</script>
-            this.source.addEventListener('user.nowPlaying', (e) => {
-              let playingInfo = JSON.parse(e.data);
-              this.user.nowPlaying = playingInfo;
-            }, false);
-
-            this.source.addEventListener('user.scrobbled', (e) => {
-              let playingInfo = JSON.parse(e.data);
-
-              this.user.scrobbled = playingInfo;
-            }, false);
-          } else {
-            // Result to xhr polling :(
-          }
-
-          this.$http.get('http://192.168.1.180:3000/lastfm', {
-            params: {
-              'user': username,
-              'type': 'user.getInfo'
-            }
-          }).then(response => {
-
-            // get body data
+						console.log(response.body);
             this.userData = response.body;
 
           }, response => {
